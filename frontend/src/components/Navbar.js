59705254755728644654098cf478aa708c90feba
unknown <swarnimsid2@gmail.com>
2023-04-05 20:00:37 +0530

    complete navbar update

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from 'react'
 import authService from "../services/authService";
 import "../App";
 
@@ -11,46 +11,46 @@ function Navbar() {
   }
   const token = authService.getCurrentUser();
   return (<div>
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div className="container-fluid">
-        <a className="navbar-brand" href="/">P&M</a>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="/">Pack&Mov</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="/">Home</a>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="/">Home</a>
             </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Services</a>
+            <li class="nav-item">
+              <a class="nav-link active" href="#">Link</a>
             </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">All</a>
+            <li class="nav-item">
+              <a class="nav-link active">Disabled</a>
             </li>
+          </ul>
+          <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
             {!token && (
-            <li className="signup-home">
-            <button type="button" class="btn btn-outline-secondary"><a id = "green1" href="/SignUp">Sign Up</a></button>
-            </li>
+              <li class="nav-but">
+                <a id="green" class="btn btn-secondary" href="/signup">SignUp</a>
+              </li>
             )}
             {!token && (
-              <li className="login-home">
-                <button type="button" class="btn btn-outline-success"><a id = "green" href="/login">Login</a></button>
-
+              <li class="nav-but">
+                <a id="green" class="btn btn-success" href="/login">Login</a>
               </li>
-              
             )}
             {token && (
               <li className="nav-item">
                 <a className="nav-link" onClick={handleLogOut}>Logout</a>
               </li>
-              
+
             )}
           </ul>
         </div>
       </div>
     </nav>
-  </div>);
+  </div>
+  )
 }
-
 export default Navbar;
